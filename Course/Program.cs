3d7fb3d91b1582dc6028be672d06efa967b65bf0
Alan <alanraysilva@hotmail.com>
2019-08-08 11:21:27 -0300

    Acrescentado mensagem de bom dia

--- Course/Program.cs
+++ Course/Program.cs
@@ -9,7 +9,8 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ola mundo");
+            Console.WriteLine("Ola mundo!");
+            Console.WriteLine("Bom dia!");
         }
     }
 }
